f791f085c3d365c87d27a6cec6de38b5a14c14ac
Lev <cjbhole@gmail.com>
2024-12-04 22:21:52 +0500

    Add new project for task 4.5

---
+++ Anketa 4.5.1/Anketa 4.5.1/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Anketa_4._5._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           (string name, string lastname, string login, int loginlength, bool petown,int age, string[] favoritecolors) User;
+            for (int k = 0; k < 3; k++)
+            {
+                Console.WriteLine("Введите своё имя");
+                User.name = Console.ReadLine();
+                Console.WriteLine("Введите свою фамилию");
+                User.lastname = Console.ReadLine();
+                string Logincorrect()
+                {
+                    Console.WriteLine("Введите логин");
+                    string login = Console.ReadLine();
+                    if (login.Length > 8)
+                    {
+                        return login;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Вы ввели неверный логин, логин должен быть больше 8 символов");
+                        Logincorrect();
+                        return login;
+                    }
+                }
+                User.login = Logincorrect();
+                User.loginlength = User.login.Length;
+                Console.WriteLine("Есть ли у вас животные");
+                string pet = Console.ReadLine();
+                if (pet == "Да")
+                {
+                    User.petown = true;
+                }
+                else
+                { User.petown = false; }
+                Console.WriteLine("Введите свой возраст");
+                User.age = int.Parse(Console.ReadLine());
+                User.favoritecolors = new string[3];
+                Console.WriteLine("Введите три своих любимых цвета");
+                for (int i = 0; i < User.favoritecolors.Length; i++)
+                {
+                    User.favoritecolors[i] = Console.ReadLine();
+                }
+            }
+        }
+    }
+}
